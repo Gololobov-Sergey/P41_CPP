56c7c805f8030eae045de1dd11b03b67a3eba018
Сергій Гололобов <gololobov.serhiy@gmail.com>
2025-01-21 20:49:31 +0200

    21.01.2025

--- P41_CPP/Source.cpp
+++ P41_CPP/Source.cpp
@@ -3,11 +3,120 @@
 
 using namespace std;
 
+
 int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	cout.setf(ios::boolalpha);
+
+
+	/// 21.01.2025 //////
+
+	/*int a, b, c;
+	cin >> a >> b >> c;
+	if (a > b && b > c)
+	{
+		cout << a << endl;
+	}
+	else
+	{
+		if (b > c)
+		{
+			cout << b << endl;
+		}
+		else
+		{
+			cout << c << endl;
+		}
+	}*/
+
+	double a = 0, b;
+	char op;
+
+	cout << "a=";
+	cin >> a;
+	cout << "(+, -, *, /): ";
+	cin >> op;
+	cout << "b= ";
+	cin >> b;
+
+	if (op == '+') 
+	{
+		cout << "rez: " << a + b << endl;
+	}
+	else if (op == '-') 
+	{
+		cout << "rez=: " << a - b << endl;
+	}
+	else if (op == '*') 
+	{
+		cout << "rez: " << a * b << endl;
+	}
+	else if (op == '/') 
+	{
+		if (b != 0) 
+		{
+			cout << "rez" << a / b << endl;
+		}
+		else 
+		{
+			cout << "error404!" << endl;
+		}
+	}
+	else {
+		cout << "error404!" << endl;
+	}
+
+
+
+
+	
+	
+
+
+	// -, +, ++, -- 
+	// = , + , -, *, /, % , += , -=, *=, /=, %=, ()
+
+	// !
+	// < , >, == , <=, >=, != , || (or), && (and), ^ (xor)
+
+	// ~ , << , >> , & , |, :: , ?: , #, ,(comma) , . ->
+		
+
+	//int a = 5, b = 4, c;
+
+	//cout << a << endl;  // 5
+	//cout << a++ << endl; // 6
+	//cout << ++a << endl; // 7
+	//cout << a << endl; // 7
+	/*a = a + b;
+	a += b;
+	a *= b;
+	a += 1;
+	a++;
+	++a;
+	a--;*/
+
+	
+
+
+	/*unsigned int a = 3000000000;
+	cout << a << endl;
+
+	const float pi = 3.141592;
+
+	sqrt(25);*/
+
+	/*int a, b, c;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	float p = (a + b + c) / 2.;
+	cout << p << endl;
+	float s = sqrt(p * (p - a) * (p - b) * (p - c));
+	cout << s << endl;*/
+
 
 	/*cout << "Hello C++" << endl;
 	cout << "Мене звати Сергій" << endl;
@@ -16,7 +125,7 @@ int main()
 	cout << true << endl;*/
 
 
-	// type name;
+	// modify type name;
 
 	// bool      - 1b
 
@@ -52,7 +161,8 @@ int main()
 	cout << "Площа, при а = " << a << ", b = " << b << " дорівнює " << s << " кв. од" << endl;*/
 
 
-	cout << 150 % 100 << endl;
+	
 
 	system("pause");
 }
+
